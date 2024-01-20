9781328400102582c8aea7c6fb254e2eef579c25
Emre Cakar <emre_147123@hotmail.com>
2024-01-20 22:07:30 +0100

    JDBCAndDatabase

--- JDBCAndDatabase/src/Main.java
+++ JDBCAndDatabase/src/Main.java
@@ -8,9 +8,38 @@ public class Main {
     public static void main(String[] args) {
         Connection connect = null;
         String sql = "SELECT * FROM employees";
+        String pstm = "INSERT INTO employees (employee_name, employee_position, employee_salary) VALUES (?,?,?)";
         try {
             connect = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
             Statement str = connect.createStatement();
+            PreparedStatement prst = connect.prepareStatement(pstm);
+
+            prst.setString(1,"Emre Çakar");
+            prst.setString(2,"Chef");
+            prst.setString(3,"100000");
+            prst.executeUpdate();
+
+            prst.setString(1,"Özgür Ün");
+            prst.setString(2,"Emlakçı");
+            prst.setString(3,"50000");
+            prst.executeUpdate();
+
+            prst.setString(1,"Şahin abi");
+            prst.setString(2,"Galerici");
+            prst.setString(3,"115000");
+            prst.executeUpdate();
+
+            prst.setString(1,"Ganyotçu Arif");
+            prst.setString(2,"Şoför");
+            prst.setString(3,"25000");
+            prst.executeUpdate();
+
+            prst.setString(1,"Recep İvedik");
+            prst.setString(2,"Yatırımcı");
+            prst.setString(3,"10000");
+            prst.executeUpdate();
+
+
             ResultSet data = str.executeQuery(sql);
             while (data.next()) {
                 System.out.println("##############################################################");
@@ -19,6 +48,8 @@ public class Main {
                 System.out.println("POZİSYONU : " + data.getString("employee_position"));
                 System.out.println("MAAŞI : " + data.getInt("employee_salary"));
             }
+            prst.close();
+            str.close();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
